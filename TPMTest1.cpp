44210754ff2a3a666875b836a77f6d883ae59b8c
Prakhar314 <54989891+Prakhar314@users.noreply.github.com>
2021-03-16 17:02:17 +0530

    git hash for files

--- TPMTest1.cpp
+++ TPMTest1.cpp
@@ -2,13 +2,43 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iterator>
+#include <vector>
 #include "stdafx.h"
 #include "Tpm2.h"
 
 using namespace std;
 using namespace TpmCpp;
 
-int main()
+Tpm2 tpm;
+
+ByteVec getBlobHash(string filename) {
+    //input as char vec
+    ifstream input(filename, ios::binary);
+    vector<char> blob(
+        (istreambuf_iterator<char>(input)),
+        (istreambuf_iterator<char>()));
+    input.close();
+
+    //make prefix
+    int si = blob.size();
+    stringstream ss;
+    ss << "blob " << si;
+    string prefix = ss.str();
+
+    //make byte vec from prefix char vec
+    ByteVec bytes(prefix.begin(), prefix.end());
+    bytes.push_back('\0');
+    bytes.insert(bytes.end(), blob.begin(), blob.end());
+    //cout << bytes << endl;
+    //get SHA1 hash
+    HashResponse h = tpm.Hash(bytes, TPM_ALG_ID::SHA1, TPM_RH_NULL);
+    return h.outHash;
+}
+
+int main(int argc, char* argv[])
 {
     TpmTcpDevice device;
     if (!device.Connect("127.0.0.1", 2321)) {
@@ -16,31 +46,18 @@ int main()
         return 0;
     }
     // Create a Tpm2 object "on top" of the device.
-    Tpm2 tpm(device);
-    // When talking to the simulator you must perform some of the startup
-    // functions that would normally happen automatically or be done by
-    // the BIOS (note: PowerOff does nothing if the TPM is already powered
-    // off, but let’s this sample run whatever the state of the TPM.)
+    tpm._SetDevice(device);
+    // startup cycle
     device.PowerOff();
     device.PowerOn();
     tpm.Startup(TPM_SU::CLEAR);
-    // Get 20 bytes of random data
-    std::vector<BYTE> rand = tpm.GetRandom(20);
-    // And print it out.
-    cout << "Random bytes: " << rand << endl;
+
+    // Get blob hash
+    //cout << argv[1] << endl;
+
+    cout << getBlobHash(argv[1]) << endl;
     // And shut down the TPM
     tpm.Shutdown(TPM_SU::CLEAR);
     device.PowerOff();
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
