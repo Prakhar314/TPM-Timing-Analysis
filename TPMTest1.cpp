1dd9bce69a1e6758f3f56ed8dd17c5f5188a89fd
Rayyan Shahid <55098643+sicario001@users.noreply.github.com>
2021-04-06 19:15:59 +0530

    Added hostSystem

--- TPMTest1.cpp
+++ TPMTest1.cpp
@@ -15,8 +15,117 @@ using namespace TpmCpp;
 static const TPMT_SYM_DEF_OBJECT Aes128Cfb{ TPM_ALG_ID::AES, 128, TPM_ALG_ID::CFB };
 Tpm2 tpm;
 TpmTcpDevice device;
-vector<pair<string, pair<int, ByteVec>>> actions_log(0);
+class hostSystem {
+private:
+    Tpm2 tpm;
+    TpmTcpDevice device;
+    vector<pair<string, pair<int, ByteVec>>> event_log{};
+    TPM_HANDLE primaryKey;
+    TPM_HANDLE aik;
+    vector<TPMS_PCR_SELECTION> pcrsToQuote = { {TPM_ALG_ID::SHA1, 0}, {TPM_ALG_ID::SHA1, 1}, {TPM_ALG_ID::SHA1, 2} };
 
+    
+    void update_event_log(string description, int pcr, ByteVec& event_data) {
+        event_log.push_back({ description, {pcr, event_data} });
+    }
+    void reset_event_log() {
+        event_log.clear();
+    }
+
+    void perform_action(string des, int pcr, ByteVec& event_data) {
+        tpm.PCR_Event(TPM_HANDLE::Pcr(pcr), event_data);
+        std::cout << des << "\n";
+        update_event_log(des, pcr, event_data);
+    }
+    void action0() {
+        string des = "Performing action 1";
+        ByteVec event_data = { 1,2,3 };
+        perform_action(des, 0, event_data);
+    }
+    void action1() {
+        string des = "Performing action 2";
+        ByteVec event_data = { 2,3,4 };
+        perform_action(des, 1, event_data);
+    }
+    void action2() {
+        string des = "Performing action 3";
+        ByteVec event_data = { 3,4,5 };
+        perform_action(des, 2, event_data);
+    }
+    TPM_HANDLE gen_prim_key() {
+        TPMT_PUBLIC storagePrimaryTemplate(TPM_ALG_ID::SHA1,
+            TPMA_OBJECT::decrypt | TPMA_OBJECT::restricted
+            | TPMA_OBJECT::fixedParent | TPMA_OBJECT::fixedTPM
+            | TPMA_OBJECT::sensitiveDataOrigin | TPMA_OBJECT::userWithAuth,
+            ByteVec(),           // No policy
+            TPMS_RSA_PARMS(Aes128Cfb, TPMS_NULL_ASYM_SCHEME(), 2048, 65537),
+            TPM2B_PUBLIC_KEY_RSA());
+        return tpm.CreatePrimary(TPM_RH::OWNER, TPMS_SENSITIVE_CREATE(), storagePrimaryTemplate, ByteVec(), vector<TpmCpp::TPMS_PCR_SELECTION>())
+            .handle;
+    }
+
+    TPM_HANDLE MakeChildSigningKey(TPM_HANDLE parent, bool restricted)
+    {
+        TPMA_OBJECT restrictedAttribute = restricted ? TPMA_OBJECT::restricted : 0;
+
+        TPMT_PUBLIC templ(TPM_ALG_ID::SHA1,
+            TPMA_OBJECT::sign | TPMA_OBJECT::fixedParent | TPMA_OBJECT::fixedTPM
+            | TPMA_OBJECT::sensitiveDataOrigin | TPMA_OBJECT::userWithAuth | restrictedAttribute,
+            ByteVec(),  // No policy
+            TPMS_RSA_PARMS(TPMT_SYM_DEF_OBJECT(), TPMS_SCHEME_RSASSA(TPM_ALG_ID::SHA1), 2048, 65537), // PKCS1.5
+            TPM2B_PUBLIC_KEY_RSA());
+
+        auto newSigningKey = tpm.Create(parent, TPMS_SENSITIVE_CREATE(), templ, ByteVec(), vector<TpmCpp::TPMS_PCR_SELECTION>());
+
+        return tpm.Load(parent, newSigningKey.outPrivate, newSigningKey.outPublic);
+    }
+
+public:
+    hostSystem() {
+        if (!device.Connect("127.0.0.1", 2321)) {
+            cerr << "Could not connect to the TPM device";
+        }
+        tpm._SetDevice(device);
+
+        // Power-cycle the simulator
+        device.PowerOff();
+        device.PowerOn();
+
+        // And startup the TPM
+        tpm.Startup(TPM_SU::CLEAR);
+        primaryKey = gen_prim_key();
+        aik = MakeChildSigningKey(primaryKey, false);
+    }
+    vector<pair<string, pair<int, ByteVec>>> requestEventLog() {
+        return event_log;
+    }
+    TpmCpp::QuoteResponse requestQuote(ByteVec Nonce) {
+        return tpm.Quote(aik, Nonce, TPMS_NULL_SIG_SCHEME(), pcrsToQuote);
+    }
+    TpmCpp::ReadPublicResponse requestPubAikKey() {
+        return tpm.ReadPublic(aik);
+    }
+    TpmCpp::PCR_ReadResponse requestPcrVal() {
+        return tpm.PCR_Read(pcrsToQuote);
+    }
+    void performActions() {
+        action0();
+        action1();
+        action2();
+    }
+};
+
+void connectSimTPM(){
+    
+    if (!device.Connect("127.0.0.1", 2321)) {
+        cerr << "Could not connect to the TPM device";
+    }
+    tpm._SetDevice(device);
+
+    device.PowerOff();
+    device.PowerOn();
+    tpm.Startup(TPM_SU::CLEAR);
+}
 
 void getBlobHash(string filename) {
     //input as char vec
@@ -238,78 +347,45 @@ void val_message() {
     std::cout << "Signature is " << (tpm._LastCommandSucceeded() ? "OK" : "BAD") << endl;
 }
 
-void update_actions_log(string description, int pcr, ByteVec& event_data) {
-    actions_log.push_back({ description, {pcr, event_data} });
-}
-void reset_actions_log() {
-    actions_log.clear();
-}
 
-void perform_action(string des, int pcr, ByteVec& event_data) {
-    tpm.PCR_Event(TPM_HANDLE::Pcr(pcr), event_data);
-    std::cout << des << "\n";
-    update_actions_log(des, pcr, event_data);
-}
-void action0() {
-    string des = "Performing action 1";
-    ByteVec event_data = { 1,2,3 };
-    perform_action(des, 0, event_data);
-}
-void action1() {
-    string des = "Performing action 2";
-    ByteVec event_data = { 2,3,4 };
-    perform_action(des, 1, event_data);
-}
-void action2() {
-    string des = "Performing action 3";
-    ByteVec event_data = { 3,4,5 };
-    perform_action(des, 2, event_data);
-}
-vector<TPM_HASH> get_pcr_vals(PCR_ReadResponse pcrVals_old) {
+vector<TPM_HASH> get_pcr_vals(PCR_ReadResponse pcrVals_old, vector<pair<string, pair<int, ByteVec>>>& event_log) {
     TPM_HASH pcrSim0(TPM_ALG_ID::SHA1, pcrVals_old.pcrValues[0]);
     TPM_HASH pcrSim1(TPM_ALG_ID::SHA1, pcrVals_old.pcrValues[1]);
     TPM_HASH pcrSim2(TPM_ALG_ID::SHA1, pcrVals_old.pcrValues[2]);
-    for (auto x : actions_log) {
+    for (auto x : event_log) {
         if (x.second.first == 0) {
-            pcrSim0.Event(TPM_HASH(TPM_ALG_ID::SHA1, x.second.second).digest);
+            pcrSim0.Event(x.second.second);
         }
         else if (x.second.first == 1) {
-            pcrSim1.Event(TPM_HASH(TPM_ALG_ID::SHA1, x.second.second).digest);
+            pcrSim1.Event(x.second.second);
         }
         else {
-            pcrSim2.Event(TPM_HASH(TPM_ALG_ID::SHA1, x.second.second).digest);
+            pcrSim2.Event(x.second.second);
         }
 
     }
     return { pcrSim0, pcrSim1, pcrSim2 };
 }
-void update_using_nonce(vector<TPM_HASH>& pcrVals_hash, ByteVec Nonce) {
-    pcrVals_hash[0].Event(Nonce);
-    pcrVals_hash[1].Event(Nonce);
-    pcrVals_hash[2].Event(Nonce);
-}
+
 void attestation() {
-    TPM_HANDLE primaryKey = gen_prim_key();
-    TPM_HANDLE aik = MakeChildSigningKey(primaryKey, false);
+    hostSystem system1;
 
     std::cout << ">> PCR Quoting" << endl;
-    vector<TPMS_PCR_SELECTION> pcrsToQuote = { {TPM_ALG_ID::SHA1, 0}, {TPM_ALG_ID::SHA1, 1}, {TPM_ALG_ID::SHA1, 2} };
-    auto pcrVals_old = tpm.PCR_Read(pcrsToQuote);
-    // Do an event to make sure the value is non-zero
-    action0();
-    action1();
-    action2();
+    auto pcrVals_old = system1.requestPcrVal();
+    
+    system1.performActions();
+    auto event_log = system1.requestEventLog();
 
     // Then read the value so that we can validate the signature later
-    auto pcrVals_hash_calc = get_pcr_vals(pcrVals_old);
+    auto pcrVals_hash_calc = get_pcr_vals(pcrVals_old, event_log);
 
     // Do the quote.  Note that we provide a nonce.
     ByteVec Nonce = Crypto::GetRand(16);
 
-    auto pubKey = tpm.ReadPublic(aik);
+    auto pubKey = system1.requestPubAikKey();
 
-    auto quote = tpm.Quote(aik, Nonce, TPMS_NULL_SIG_SCHEME(), pcrsToQuote);
-    bool sigOk = pubKey.outPublic.ValidateQuote(tpm.PCR_Read(pcrsToQuote), Nonce, quote);
+    auto quote = system1.requestQuote(Nonce);
+    bool sigOk = pubKey.outPublic.ValidateQuote(system1.requestPcrVal(), Nonce, quote);
     if (sigOk)
         std::cout << "The quote was verified correctly" << endl;
     _ASSERT(sigOk);
@@ -326,24 +402,15 @@ void attestation() {
     if (d_quote == d_calc) {
         cout << "pcr values match" << endl;
     }
+    else {
+        cout << "Attestation failed" << endl;
+    }
 }
+
 
 int main(int argc, char* argv[])
 {
-    // TpmTbsDevice device;
-    TpmTcpDevice device;
-    // if (!device.Connect()) {
-    if (!device.Connect("127.0.0.1", 2321)) {
-        cerr << "Could not connect to the TPM device";
-    }
-    tpm._SetDevice(device);
-
-    // Power-cycle the simulator
-    device.PowerOff();
-    device.PowerOn();
-
-    // And startup the TPM
-    tpm.Startup(TPM_SU::CLEAR);
+    
     if (argc == 2) {
         string s;
         int i = 0;
@@ -351,22 +418,27 @@ int main(int argc, char* argv[])
         case 0:
             std::cout << "Enter file name: ";
             std::cin >> s;
+            connectSimTPM();
             getBlobHash(s);
             break;
         case 1:
             std::cout << "Enter number of iterations: ";
             std::cin >> i;
+            connectSimTPM();
             write_csv(sign_multiple(generate_ecdsa_key(), i), "out.csv");
             break;
         case 2:
+            connectSimTPM();
             sign_message();
             break;
         case 3:
+            connectSimTPM();
             val_message();
             break;
         case 4:
             std::cout << "Enter file name: ";
             std::cin >> s;
+            connectSimTPM();
             encrypt_decrypt(s);
             break;
         case 5:
