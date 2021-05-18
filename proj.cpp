066429c376d49bfd95d9c9e2f5551c33e90b45f4
Prakhar Jagwani <prakharjagwani7@gmail.com>
2021-05-18 12:27:55 +0530

    change ops

--- proj.cpp
+++ proj.cpp
@@ -32,22 +32,27 @@ int getTime(){
 int main(){
     connectTPM();
 
-    int times[] = {0,0,0,0};
+    int times[] = {0,0,0,0,0};
     vector<int> tests = {16,512,1024};
     int iterations = 1000;
     for(int i = 0 ; i < iterations;i++){
-        auto pcr_to_read = TPMS_PCR_SELECTION::GetSelectionArray(TPM_ALG_ID::SHA256,rand()%20);
+
         int start_time = getTime();
-        tpm.PCR_Read(pcr_to_read).pcrValues;
+        tpm.ReadClock();
         times[0] += (getTime() - start_time);
+
+        start_time = getTime();
+        tpm.PCR_Read({}).pcrValues;
+        times[1] += (getTime() - start_time);
+        
         for(int tn = 0;tn<(int)tests.size();tn++){
             auto b = tests[tn];
             ByteVec bytes = Crypto::GetRand(b);
             start_time = getTime();
             tpm.Hash(bytes,TPM_ALG_ID::SHA256,TPM_RH_NULL).outHash;
-            times[tn+1] += (getTime() - start_time);
+            times[tn+2] += (getTime() - start_time);
         }
     }
-    cout << 1.0*times[0]/iterations << "\t" << 1.0*times[1]/iterations << "\t" << 1.0*times[2]/iterations << "\t" << 1.0*times[3]/iterations << endl;
+    cout << 1.0*times[0]/iterations << "\t" << 1.0*times[1]/iterations << "\t" << 1.0*times[2]/iterations << "\t" << 1.0*times[3]/iterations<<"\t"<< 1.0*times[4]/iterations << "\t" << endl;
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-echo -e "%\tPCR_Read\t16\t512\t1024" > logs.tsv
+echo -e "%\tReadClock\tPCR_Read\t16\t512\t1024" > logs.tsv
 for i in 1 2 3 4 5 6 7 8 9 10
 do
     echo "running at $i%"
