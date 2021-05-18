ff568b93750c6bac323244d21b3cb2ddbfe79d00
Prakhar Jagwani <prakharjagwani7@gmail.com>
2021-05-18 10:05:59 +0530

    tsv

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-echo -e "%\tPCR_Read\t16\t512\t1024" > logs.csv
+echo -e "%\tPCR_Read\t16\t512\t1024" > logs.tsv
 for i in 1 2 3 4 5 6 7 8 9 10
 do
     echo "running at $i%"
-    echo -en "$i%\t" >> logs.csv
-    cpulimit -l $i -i ./proj >> logs.csv
+    echo -en "$i%\t" >> logs.tsv
+    cpulimit -l $i -i ./proj >> logs.tsv
 done
